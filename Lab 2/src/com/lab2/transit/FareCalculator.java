19db21dff3650db868e37e677f06b4c375757e51
tug18408 <tug18408@temple.edu>
2015-11-17 18:33:58 -0500

    Committed.

--- Lab 2/src/com/lab2/transit/FareCalculator.java
+++ Lab 2/src/com/lab2/transit/FareCalculator.java
@@ -1,7 +1,66 @@
 package com.lab2.transit;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized.Parameters;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class FareCalculator {
 	
+	private static final double DELTA = 1e-15;
+	
+	private double expected;
+	private int age;
+	private String time;
+	private boolean isHoliday;
+	
+	
+	public FareCalculatorTest(double expected, int age, String time, boolean isHoliday) 
+	{
+		this.expected = expected;
+		this.age = age;
+		this.time = time;
+		this.isHoliday = isHoliday;
+	}
+	
+	@Parameters
+	public static Collection<Object[]> testParams() {
+		return Arrays.asList(new Object[][] {
+				{0.0, 5, "6:59", false},
+				{2.5, 5, "7:00", false},
+				{0.0, 5, "9:01", false},
+				
+				{2.5, 6, "6:59", false},
+				{2.5, 6, "7:00", false},
+				{2.5, 6, "9:01", false},
+				
+				{0.0, 5, "6:59", true},
+				{0.0, 5, "7:00", true},
+				{0.0, 5, "9:01", true},
+				
+				{2.5, 6, "6:59", true},
+				{2.5, 6, "7:00", true},
+				{2.5, 6, "9:01", true},
+				
+				{2.5, 64, "6:59", false},
+				{2.5, 64, "7:00", false},
+				{2.5, 64, "9:01", false},
+				
+				{0.0, 65, "6:59", false},
+				{2.5, 65, "7:00", false},
+				{0.0, 65, "9:01", false},
+				
+				{2.5, 64, "6:59", true},
+				{2.5, 64, "7:00", true},
+				{2.5, 64, "9:01", true},
+				
+				{0.0, 65, "6:59", true},
+				{0.0, 65, "7:00", true},
+				{0.0, 65, "9:01", true},
+				
+				
+		});
+	}
 	/**
 	 * 
 	 * @param age - The age of the person
---
+++ Lab 2/test/com/lab2/transit/CalculateFareTest.java
@@ -0,0 +1,14 @@
+package com.lab2.transit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculateFareTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
